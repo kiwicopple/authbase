8764dbbd5d4d931ac4d2d4eaec8c4e703c65b6d0
Paul Copplestone <pcopplestone@gmail.com>
2019-07-21 22:10:57 +0800

    Adding multitenant

--- database/schema/00_init.sql
+++ database/schema/00_init.sql
@@ -3,16 +3,17 @@
 CREATE SCHEMA IF NOT EXISTS public;
 CREATE SCHEMA IF NOT EXISTS secure;
 
--- pgcrypto extension
+-- Extensions
 
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- the names "anon" and "authenticator" are configurable and not
 -- sacred, we simply choose them for clarity
 create role anon;
 create role authenticator noinherit; -- role to “become” other users to service authenticated HTTP requests
-grant anon to authenticator;
 
+grant anon to authenticator;
 grant usage on schema public, secure to anon;
 
 
